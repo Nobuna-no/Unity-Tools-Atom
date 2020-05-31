8f6647c542da9b3bf9b71630c1c4b5aae05087f5
Morgan Hoarau <morgansb.hoarau@gmail.com>
2020-05-31 14:59:34 +0200

    [Add] Utils script generator

---
+++ Core/File/ScriptGenerator.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+using System.Text;
+
+using UnityEditor;
+
+namespace UnityTools.Atom
+{
+    public class ScriptGeneratorEditor : EditorWindow
+    {
+        #region PARAMETERS
+        private static string ClassName = "New Class Name";
+        private static string pathName = "Assets/";
+
+        public static ScriptGeneratorDescriptorAsset Descriptor;
+        #endregion
+
+        #region UNITY METHODS
+        private void OnGUI()
+        {
+            if (Descriptor == null)
+            {
+                GUILayout.Box("Please open an ScriptGeneratorDescriptorAsset!");
+                return;
+            }
+
+            ClassName = GUILayout.TextField(ClassName);
+            pathName = GUILayout.TextField(pathName);
+
+            if (GUILayout.Button("Generate"))
+            {
+                if (ClassName.Length > 0)
+                {
+                    foreach (FileDescription data in Descriptor.DescriptionOfFileToGenerate)
+                    {
+                        string currentName = ClassName.Replace(" ", "");
+                        currentName = data.FilePrefix + currentName + data.FileSuffix;
+
+                        pathName = pathName.Replace(" ", "");
+                        bool needLastSlash = pathName[pathName.Length - 1] != '/';
+                        if (needLastSlash)
+                        {
+                            pathName += '/';
+                        }
+                        // Create folder if required.
+                        DirectoryInfo di = Directory.CreateDirectory(pathName.Remove(pathName.Length - 1, 1));
+
+                        string copyPath = pathName + currentName + ".cs";
+
+
+                        Debug.Log("Creating C# file: " + copyPath);
+                        if (File.Exists(copyPath) == false)
+                        {
+                            //string formatString = data.Template.Replace("\t", "   ");
+                            string formatString = data.Template.Replace("<CLASSNAME>", ClassName.Replace(" ", ""));
+                            string[] contentLines = formatString.Split('\n');
+
+                            // do not overwrite
+                            using (StreamWriter outfile = new StreamWriter(copyPath))
+                            {
+                                for (int i = 0, c = contentLines.Length; i < c; ++i)
+                                {
+                                    outfile.WriteLine(contentLines[i].Replace("\n", "").Replace("\r", ""));
+                                }
+                            }//File written
+                        }
+                    }
+                    AssetDatabase.Refresh();
+                }
+            }
+        }
+        #endregion
+
+        #region PUBLIC METHODS
+        [MenuItem("Window/Script Generator Editor")]
+        public static ScriptGeneratorEditor OpenGenerateScript()
+        {
+            return EditorWindow.GetWindow<ScriptGeneratorEditor>("Script Generator");
+        }
+
+        [UnityEditor.Callbacks.OnOpenAsset(1)]
+        public static bool OnOpenDatabase(int instanceID, int line)
+        {
+            ScriptGeneratorDescriptorAsset data = EditorUtility.InstanceIDToObject(instanceID) as ScriptGeneratorDescriptorAsset;
+
+            if (data != null)
+            {
+                Descriptor = data;
+                pathName = Descriptor.Destination;
+                ScriptGeneratorEditor editorWindow = OpenGenerateScript();
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+    }
+}
---
+++ Core/File/ScriptGeneratorDescriptorAsset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityTools.Atom
+{
+    #region STRUCTS
+    [System.Serializable]
+    public class FileDescription
+    {
+        public string FilePrefix;
+        public string FileSuffix;
+        [TextArea(1, 10)]
+        public string Template = "using UnityEngine;\nusing System.Collections;\npublic class <CLASSNAME> : MonoBehaviour{}";
+    }
+    #endregion
+
+    [CreateAssetMenu(fileName = "[SGDescriptor]-", menuName = "Unity Tools/Atom/Core/File/ScriptGeneratorDescriptor")]
+    public class ScriptGeneratorDescriptorAsset : ScriptableObject
+    {
+        #region PARAMETERS
+        public List<FileDescription> DescriptionOfFileToGenerate = new List<FileDescription>(1);
+        public string Destination = "Assets/";
+        #endregion
+    }
+}
